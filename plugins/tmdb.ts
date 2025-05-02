16b083a4e3ba3f3f401a764f77ac91ce8791d23e
adurand <alex.durand.dev@gmail.com>
2025-05-02 12:39:47 +0200

    update readme

--- plugins/tmdb.ts
+++ plugins/tmdb.ts
@@ -4,5 +4,9 @@ export default defineNuxtPlugin(() => {
     console.log("Initializing TMDB client...")
     const config = useRuntimeConfig()
     const tmdbApiKey = config.public.tmdbApiKey
+
+    if (!tmdbApiKey) {
+        throw new Error("TMDB API key is not set in the runtime config.")
+    }
     initTMDBClient(tmdbApiKey)
 })
